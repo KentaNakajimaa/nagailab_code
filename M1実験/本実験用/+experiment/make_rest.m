5fbdb0adb8f68e802dafc8b5833e67210e968e51
root <root@nakajima-ROG-Flow-X13-GV301QH-GV301QH>
2023-07-06 13:10:24 +0900

    0706

---
+++ M1実験/本実験用/+experiment/make_rest.m
@@ -0,0 +1,17 @@
+function t = make_rest(mainWindow,black,rect_position,xcenter,ycenter,key_start)
+    myText = double('2を押して開始');
+    DrawFormattedText(mainWindow, myText, 'center', 'center', [200 200 200]);
+    make_triggur(mainWindow,black,rect_position);
+    Screen('Flip', mainWindow);
+    while 1
+        [~ ,~ ,KeyCode] = KbCheck;
+        if KeyCode(key_start)
+            break
+        end
+    end
+    while KbCheck; end
+ 
+    Screen('DrawText', mainWindow,'+',xcenter-30, ycenter-30);
+    make_triggur(mainWindow,black,rect_position);
+    t = Screen('Flip', mainWindow);
+end
