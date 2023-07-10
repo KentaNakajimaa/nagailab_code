e9e062f2a19f691d2d72fc2b2c506fddefdd19a9
root <root@nakajima-ROG-Flow-X13-GV301QH-GV301QH>
2023-07-10 18:43:21 +0900

    07/10

---
+++ +M1_experiment/+experiment/make_blank.m
@@ -0,0 +1,5 @@
+function t = make_blank(mainWindow,xcenter,ycenter,black,rect_position,time_p,time_n)
+    Screen('DrawText', mainWindow,'+',xcenter-30, ycenter-30);
+    M1_experiment.experiment.make_triggur(mainWindow,black,rect_position);
+    t = Screen('Flip', mainWindow,time_p + time_n);
+end
--- +M1_experiment/+main/test.m
+++ +M1_experiment/+main/test.m
@@ -1,26 +1,7 @@
 %% 事前準備
 clear;
 ListenChar(2);
-
-stimulisize = 5;    % 試行回数設定    :   600
-gray = [128 128 128];
-black = [0 0 0];
-% 反応用キー設定
-KbName('UnifyKeyNames');
-%rightKey = KbName('6');
-%leftKey = KbName('4');
-rightKey = KbName('s');
-leftKey = KbName('a');
-
-
-% parameter setting
-
-rect_color = [255 255 255];
-%refreshrate = 60;
-
-%load('/home/nagailab/ドキュメント/nakajima/parameta.mat')
-
-
+stimulisize = 40; 
 %% input file open
 %change type_num and name 
 type = [("gloss"),("moist"),("beautiful"),("luxury")];
@@ -29,7 +10,17 @@ type_txt = [("光沢感"),("しっとり感"),("綺麗"),("高級感")];
 
 type_num =1;
 name = ('test');
-
+%ID = 1;
+%while 1
+%    filename = ['/home/nakajima/ドキュメント/研究室/研究データ/M1実験/' char(type(type_num)) '_' name '_result_' num2str(ID) '.txt'];
+%    if isfile(filename) == 1
+%        ID = ID + 1;
+%        continue;
+%    end
+%    fileID = fopen(filename,'W');
+%    resultID = ['/home/nakajima/ドキュメント/研究室/研究データ/M1実験/' char(type(type_num)) '_' name '_result_' num2str(ID) '.mat'];
+%    result = zeros(stimulisize,2);
+%end
 %filename = ['/home/nagailab/ドキュメント/nakajima/ex_' char(type(type_num)) '/' char(type(type_num)) '_' name '_result.txt'];
 %filename = ['/home/nakajima/ドキュメント/研究室/研究データ/M1実験/予備実験/ex_' char(type(type_num)) '/long/' char(type(type_num)) '_' name '_result.txt'];
 filename = '/home/nakajima/ドキュメント/研究室/研究データ/M1実験/test.txt';
@@ -45,8 +36,34 @@ result = zeros(stimulisize,2);
 %g = readtable('/home/nagailab/ドキュメント/g.csv');
 %b = readtable('/home/nagailab/ドキュメント/b.csv');
 
+stimulisize = 40;    % 試行回数設定    :   600
+stimuli_len = 20;
+gray = [128 128 128];
+black = [0 0 0];
+% 反応用キー設定
+KbName('UnifyKeyNames');
+%rightKey = KbName('6');
+%leftKey = KbName('4');
+rightKey = KbName('s');
+leftKey = KbName('a');
+% parameter setting
+
+rect_color = [255 255 255];
+%refreshrate = 60;
+
+%load('/home/nagailab/ドキュメント/nakajima/parameta.mat')
+
+
+
 %gamma_converter = tnt.GammaConverter(r, g, b);
 %close all
+
+% time of image
+%exp_time.before = 1/refreshrate *2;
+exp_time.first = 1.2;
+exp_time.present = [1.0,0.15,0.1];%0.5
+exp_time.blank = [1.0,1.85,1.9];
+img_no = 20;
 %% PTBウインドウを開く
 %Screen('Preference', 'SkipSyncTests', 1);
 % open PTB3 
@@ -81,6 +98,8 @@ while KbCheck; end
 % 画像表示
     
 order = randperm(stimulisize);
+order1 = rem(order,stimuli_len) +1;
+order2 = rem(order,3) + 1;
 %image position
 image_position = [xcenter-256 ycenter-192 xcenter+256 ycenter+192];
 % triggur position
@@ -89,11 +108,6 @@ image_position = [xcenter-256 ycenter-192 xcenter+256 ycenter+192];
 % for test
 rect_position_right = [xcenter+400 ycenter+400 xcenter+500 ycenter+500];
 
-% time of image
-%exp_time.before = 1/refreshrate *2;
-exp_time.first = 1.2;
-exp_time.present = 1.0;%0.5
-exp_time.blank1 = 1.0;
 
 %try
 Screen('TextFont',mainWindow,'-:lang=ja');
@@ -105,11 +119,11 @@ for i = 1:stimulisize
     Screen('Close');
     %%rest
     if rem(i ,20) == 1
-        exp_time.flip = make_rest(mainWindow,black,rect_position_right,xcenter,ycenter,space);
+        exp_time.flip = M1_experiment.experiment.make_rest(mainWindow,black,rect_position_right,xcenter,ycenter,space);
         
     else
         %blank
-        exp_time.flip = make_blank(mainWindow,xcenter,ycenter,black,rect_position_right,0,0);    
+        exp_time.flip = M1_experiment.experiment.make_blank(mainWindow,xcenter,ycenter,black,rect_position_right,0,0);    
     end
     
     %target image
@@ -121,24 +135,25 @@ for i = 1:stimulisize
         %ip = 2;%left
     %end
     %imName1 = ['/home/nagailab/ドキュメント/nakajima/stimuli/' num2str(order(i)) '.jpeg'];
-    imName1 = ['/home/nakajima/ドキュメント/研究室/研究データ/stimuli/' num2str(order(i)) '.jpeg'];
+    imName1 = ['/home/nakajima/ドキュメント/研究室/研究データ/stimuli/' num2str(order1(i)) '.jpeg'];
     imdata1 = imread(imName1);    
-    fprintf(fileID,'%d, ',order(i));    
+    fprintf(fileID,'%d, ',order1(i));    
     
-    exp_time.flip = show_image(mainWindow,imdata1,image_position,xcenter,ycenter,rect_color,rect_position_right,exp_time.flip,exp_time.first);    
+    exp_time.flip = M1_experiment.experiment.show_image(mainWindow,imdata1,image_position,xcenter,ycenter,rect_color,rect_position_right,exp_time.flip,exp_time.first);    
     
     %for blank
-    exp_time.flip = make_blank(mainWindow,xcenter,ycenter,black,rect_position_right,exp_time.flip,exp_time.present);
+    exp_time.flip = M1_experiment.experiment.make_blank(mainWindow,xcenter,ycenter,black,rect_position_right,exp_time.flip,exp_time.present(order2(i)));
     
     % question
     Screen('close');
     %Screen('TextFont',mainWindow,'-:lang=ja');
     %Screen('TextSize', mainWindow, 50);
-    exp_time.flip = question(mainWindow,type_txt(type_num),black,rect_position_right,exp_time.flip,exp_time.blank1);
+    exp_time.flip = M1_experiment.experiment.question(mainWindow,type_txt(type_num),black,rect_position_right,exp_time.flip,exp_time.blank(order2(i)));
     %answer
-    result(i,2) = wait_response(fileID,leftKey,rightKey);
+    result(i,2) = M1_experiment.experiment.wait_response(fileID,leftKey,rightKey);
     %%remain result 
-    result(i,1) = order(i);
+    result(i,1) = order1(i);
+    result(i,3) = order2(i);
     
 end
 
